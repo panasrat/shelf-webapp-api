d382c89d37524ddae43fd07ea39fa90cb147a303
Panas Rattanasuwan <rattanasuwan.pn@gmail.com>
2021-03-16 12:44:02 +0700

    add: functions - login

---
+++ dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    items: {
+        userHandle: 'user',
+        body: 'i am an item',
+        createdAt: '2020-03-15T11:46:01.018Z',
+        likeCount: 5,
+        commentCount: 2
+    }
+}
