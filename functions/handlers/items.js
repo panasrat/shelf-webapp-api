c8db40339bb49e6d96df6a24a070313fa5bb6a77
Panas Rattanasuwan <rattanasuwan.pn@gmail.com>
2021-03-20 01:45:53 +0700

    fix: minor fix

--- functions/handlers/items.js
+++ functions/handlers/items.js
@@ -12,6 +12,9 @@ exports.getAllItems = (req, res) => {
           body: doc.data().body,
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage,
         });
       });
       return res.json(items);
@@ -78,7 +81,7 @@ exports.getItem = (req, res) => {
 
 exports.commentOnItem = (req, res) => {
   if (req.body.body.trim() === '')
-    return res.status(400).json({ error: 'Must not be empty' });
+    return res.status(400).json({ comment: 'Must not be empty' });
 
   const newComment = {
     body: req.body.body,
